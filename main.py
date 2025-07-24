22f4312b80102e3db0a34932301f0b007bc5c488
shawn <shawnhutchins1@gmail.com>
2025-07-24 19:59:26 -0400

    Pulling quiz questions from api

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+#from data import question_data
 from question_model import Question
-from data import question_data
 from quiz_brain import QuizBrain
+import requests
+
+request = requests.get('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=boolean')
+requested_questions = request.json()["results"]
 
 question_bank = []
 
-for question in question_data:
-    new_question = Question(question["text"], question["answer"])
+for question in requested_questions:
+    new_question = Question(requested_questions[len(question_bank)]["question"],
+                            requested_questions[len(question_bank)]["correct_answer"])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
