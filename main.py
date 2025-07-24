36b13b7df35abab82619a979816ac869e07c6cae
shawn <shawnhutchins1@gmail.com>
2025-07-23 21:37:57 -0400

    Finished main lesson

--- main.py
+++ main.py
@@ -9,4 +9,9 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
